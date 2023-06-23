55a70305ed6843e4ef6524228ccc0ca86379c14d
Steffen Peikert <steffen.peikert@gmail.com>
2023-06-23 19:16:42 +0200

    Add and ran clang-format

--- include/renderAPI/RenderAPI.hpp
+++ include/renderAPI/RenderAPI.hpp
@@ -1,6 +1,6 @@
 //
-// @brief   
-// @details 
+// @brief
+// @details
 // @author  Steffen Peikert (ch3ll)
 // @email   Horizon@ch3ll.com
 // @version 1.0.0
@@ -13,46 +13,46 @@
 
 namespace HORIZON::RENDERAPI
 {
-    // TODO: shared from this?
-    class RenderAPI
-    {
-    public:
-        /*!
-         * @brief The returning function pointer for the specified function name
-         */
-        using APIFunctionPointer = void (*)();
-        /*!
-         * @brief The API function loader. Requires a function name or identifier and returns the corresponding function pointer.
-         */
-        using APIFunctionLoader = APIFunctionPointer (*)(char const*);
+	// TODO: shared from this?
+	class RenderAPI
+	{
+	public:
+		/*!
+		 * @brief The returning function pointer for the specified function name
+		 */
+		using APIFunctionPointer = void (*)();
+		/*!
+		 * @brief The API function loader. Requires a function name or identifier and returns the corresponding function
+		 * pointer.
+		 */
+		using APIFunctionLoader  = APIFunctionPointer (*)(char const*);
 
-    public:
-        /*!
-         * @brief Initialises the render api. This is guaranteed to be called on the thread with the context.
-         * @param loader The api function loader provided by the window library. If no loader is available, nullptr is a valid parameter as well.
-         * @return True if the api was initialised successfully.
-         */
-        virtual bool Initialise(APIFunctionLoader&& loader) noexcept = 0;
+	public:
+		/*!
+		 * @brief Initialises the render api. This is guaranteed to be called on the thread with the context.
+		 * @param loader The api function loader provided by the window library. If no loader is available, nullptr is a
+		 * valid parameter as well.
+		 * @return True if the api was initialised successfully.
+		 */
+		virtual bool Initialise(APIFunctionLoader&& loader) noexcept = 0;
 
-        virtual void Destroy() noexcept
-        { }
+		virtual void Destroy() noexcept { }
 
+		/*!
+		 * @brief Callback when the context changes the thread owner. This will be called on the new thread.
+		 */
+		virtual void OnNewThread() noexcept = 0;
 
-        /*!
-         * @brief Callback when the context changes the thread owner. This will be called on the new thread.
-         */
-        virtual void OnNewThread() noexcept = 0;
+		/*!
+		 * @brief Callback when the context changes the thread owner. This will be called on the releasing thread.
+		 */
+		virtual void OnThreadRelease() noexcept = 0;
 
-        /*!
-         * @brief Callback when the context changes the thread owner. This will be called on the releasing thread.
-         */
-        virtual void OnThreadRelease() noexcept = 0;
+		/*!
+		 * @brief Makes this context the current global context.
+		 */
+		virtual void MakeGlobal() noexcept = 0;
 
-        /*!
-         * @brief Makes this context the current global context.
-         */
-        virtual void MakeGlobal() noexcept = 0;
-
-        // TODO: get global context?
-    };
-}
+		// TODO: get global context?
+	};
+}  // namespace HORIZON::RENDERAPI
